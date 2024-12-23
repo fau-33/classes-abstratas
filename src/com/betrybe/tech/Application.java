a9aa6af935c43555331d3c5e377f2218f035e699
Flávio Félix <fau_felix30@hotmail.com>
2024-12-23 20:06:52 -0300

    Done classes abstradas

--- src/com/betrybe/tech/Application.java
+++ src/com/betrybe/tech/Application.java
@@ -3,6 +3,14 @@ package com.betrybe.tech;
 public class Application {
     public static void main(String[] args) {
         Laptop laptop = new Laptop("Samsung", 100, 14);
-        laptop.bootUp();
+        Desktop desktop = new Desktop("Dell",500);
+
+        checkDevice(laptop);
+        checkDevice(desktop);
+    }
+
+    public static void checkDevice(ComputerDevice device){
+        System.out.println("Verificando dispositivo de marca " + device.getBrand());
+        device.bootUp();
     }
 }
